140c0318d6aecbaa92fa1ba22360886b9d93e310
kobosky <117521539+kobosky@users.noreply.github.com>
2023-08-10 21:43:13 +0100

    Update outputs.tf

--- project/outputs.tf
+++ project/outputs.tf
@@ -2,5 +2,5 @@
 # This output will provide the DNS name of the Application Load Balancer
 output "alb_dns_name" {
     # Set the value of the output to the DNS name of the load balancer
-    value = "http://${aws_alb.application_load_balancer.dns_name}"
+    value = "http://${aws_lb.application_load_balancer.dns_name}"
 }
