db92fe22819f57e007644f715ab58c5561b6ac55
kobosky <117521539+kobosky@users.noreply.github.com>
2024-02-25 01:35:01 +0000

    Update backend.tf

--- project/backend.tf
+++ project/backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "dosignage-terraform-state"
+    bucket         = "dosignage-terraform-state-1"
     key            = "terraform-module/signage/terraform.tfstate"
     region         = "eu-west-2"
     #profile        = "terraform"
