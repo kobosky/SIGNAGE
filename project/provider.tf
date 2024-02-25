91cd22a013cba953bdd06692b92e73ed52b64cd6
kobosky <117521539+kobosky@users.noreply.github.com>
2024-02-25 04:43:05 +0000

    Update provider.tf

--- project/provider.tf
+++ project/provider.tf
@@ -5,5 +5,5 @@
 
 provider "aws" {
   region  = var.aws_region
-  shared_credentials_files = ["C:/Users/Kobosky/.aws/credentials"]
+  # shared_credentials_files = ["C:/Users/Kobosky/.aws/credentials"]
 }
